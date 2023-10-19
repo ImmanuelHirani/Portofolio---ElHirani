d18ea646743cf2cf8aeb69b0da19046dcd9d1952
immanuelHirani <gabutb688@gmail.com>
2023-10-19 13:07:48 +0700

    Finisihing Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,34 @@ module.exports = {
 
 
     extend: {
+
+      keyframes: {
+        mousebc: {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(2.5px)',
+          },
+        },
+      },
+
+      animation: {
+        mosueBounce: 'mousebc 1s infinite',
+      },
+
       fontFamily: {
         'Poppins': ['Poppins', 'sans-serif']
       },
+
       colors: {
         'Green-saber'  : '#11AF47',
-        'Black-storke' : '#3D3C3C'
+        'Black-storke' : '#3D3C3C',
+        'Black-Nav'    : '#121212'
       },
 
       screens: {
+        "xl" : "1200px",
         "2xl": "1320px",
         "3xl": '1600px',
       },
